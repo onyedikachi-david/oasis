74bd7d752e17afef3583e06705fb80f35d967a11
David Anyatonwu <davidanyatonwu@gmail.com>
2023-07-30 22:16:44 +0100

    Done? Almost

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -34,29 +34,6 @@ const Hero = () => {
               <button className={style.button}>Create</button>
             </div>
           </div>
-          <div className={style.cardContainer}>
-            <img
-              className="rounded-t-lg"
-              src="https://images.saatchiart.com/saatchi/971395/art/8312972/7377855-HSC00001-7.jpg"
-              alt=""
-              width={300}
-            />
-            <div className={style.infoContainer}>
-              <img
-                className="h-[2.25rem] rounded-full"
-                src="https://lh3.googleusercontent.com/qQj55gGIWmT1EnMmGQBNUpIaj0qTyg4YZSQ2ymJVvwr_mXXjuFiHJG9d3MRgj5DVgyLa69u8Tq9ijSm_stsph8YmIJlJQ1e7n6xj=s64"
-                alt=""
-              />
-              <div className={style.author}>
-                <div className={style.name}>Kaji</div>
-                <a
-                  className="text-[#1868b7]"
-                  href="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/2324922113504035910649522729980423429926362207300810036887725141691069366277"
-                >
-Build Trust with Your Customers                </a>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
--- app/pages/index.js
+++ app/pages/index.js
@@ -1,15 +1,36 @@
-import Image from "next/image";
+import { useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import { AppProvider } from "@/context/context";
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const endpoint = clusterApiUrl(WalletAdapterNetwork.Devnet);
+
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
   return (
     <>
-      <Header />
-      <Hero />
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            <AppProvider>
+              <Header />
+              <Hero />
+            </AppProvider>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
     </>
   );
 }
